d224071876976062f5ee1e8096f2719db7052f64
w4nts <121334927+w4nts@users.noreply.github.com>
2023-01-17 00:49:58 -0500

    Update MaliDmResponder.py

--- MaliDmResponder.py
+++ MaliDmResponder.py
@@ -23,9 +23,9 @@ async def on_ready():
 @client.event        
 async def on_message(message):
         if isinstance(message.channel, discord.DMChannel) or isinstance(message.channel, discord.GroupChannel):
- if message.author.id != client.user.id:
-                        await message.reply(replyMessage)
-                        print('Replied to %s.' % message.author.name)
+          if message.author.id != client.user.id:
+            await message.reply(replyMessage)
+            print('Replied to %s.' % message.author.name)
 
 if __name__ == '__main__':
     client.run(token, bot=False)
